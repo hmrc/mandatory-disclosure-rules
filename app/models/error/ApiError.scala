f80c20ecea71e9d29e6ba431e0202e35df748f6f
Shailesh <shaileshp@gmail.com>
2022-01-11 13:46:02 +0000

    rename the variable

--- app/models/error/ApiError.scala
+++ app/models/error/ApiError.scala
@@ -18,5 +18,5 @@ package models.error
 
 sealed trait ApiError
 
-case class ReadSubscriptionError(int: Int) extends ApiError
-case class UpdateSubscriptionError(int: Int) extends ApiError
+case class ReadSubscriptionError(status: Int) extends ApiError
+case class UpdateSubscriptionError(status: Int) extends ApiError
