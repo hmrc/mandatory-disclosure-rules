c5e4239ccf367add24e1a73861bbfd554a9ae845
ashwinirudrappa <11343826+ashwinirudrappa@users.noreply.github.com>
2022-03-08 10:48:43 +0000

    DAC6-1505: Validate EIS response against the XSD

--- test/controllers/EISResponseControllerSpec.scala
+++ test/controllers/EISResponseControllerSpec.scala
@@ -53,7 +53,7 @@ class EISResponseControllerSpec extends SpecBase with BeforeAndAfterEach {
     .build()
 
   private val randomUUID = UUID.randomUUID()
-  val xml: NodeSeq = <BREResponse>
+  val xml: NodeSeq = <cadx:BREResponse xmlns:cadx="http://www.hmrc.gsi.gov.uk/mdr/cadx">
     <requestCommon>
       <receiptDate>2001-12-17T09:30:47Z</receiptDate>
       <regime>MDR</regime>
@@ -61,24 +61,24 @@ class EISResponseControllerSpec extends SpecBase with BeforeAndAfterEach {
       <schemaVersion>1.0.0</schemaVersion>
     </requestCommon>
     <requestDetail>
-      <gsm:GenericStatusMessage>
-        <gsm:ValidationErrors>
-          <gsm:FileError>
-            <gsm:Code>50009</gsm:Code>
-            <gsm:Details Language="EN">Duplicate message ref ID</gsm:Details>
-          </gsm:FileError>
-          <gsm:RecordError>
-            <gsm:Code>80010</gsm:Code>
-            <gsm:Details Language="EN">A message can contain either new records (OECD1) or corrections/deletions (OECD2 and OECD3), but cannot contain a mixture of both</gsm:Details>
-            <gsm:DocRefIDInError>asjdhjjhjssjhdjshdAJGSJJS</gsm:DocRefIDInError>
-          </gsm:RecordError>
-        </gsm:ValidationErrors>
-        <gsm:ValidationResult>
-          <gsm:Status>Rejected</gsm:Status>
-        </gsm:ValidationResult>
-      </gsm:GenericStatusMessage>
+      <GenericStatusMessage>
+        <ValidationErrors>
+          <FileError>
+            <Code>50009</Code>
+            <Details Language="EN">Duplicate message ref ID</Details>
+          </FileError>
+          <RecordError>
+            <Code>80010</Code>
+            <Details Language="EN">A message can contain either new records (OECD1) or corrections/deletions (OECD2 and OECD3), but cannot contain a mixture of both</Details>
+            <DocRefIDInError>asjdhjjhjssjhdjshdAJGSJJS</DocRefIDInError>
+          </RecordError>
+        </ValidationErrors>
+        <ValidationResult>
+          <Status>Rejected</Status>
+        </ValidationResult>
+      </GenericStatusMessage>
     </requestDetail>
-  </BREResponse>
+  </cadx:BREResponse>
 
   "EISResponseController" - {
     "must return ok when input xml is valid" in {
