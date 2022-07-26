2b72f9b7949897d0f402aaf7b02388207e3cac6e
Shailesh Patil <shaileshp@gmail.com>
2022-07-26 14:25:10 +0200

    Dac6-1911 (#91)
    * DAC6-1991-Upgrade-Mongo
    
    * DAC6-1991-Upgrade-Mongo
    
    * rebase main

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,7 +7,7 @@ object AppDependencies {
 
   val compile = Seq(
     "uk.gov.hmrc"       %% "bootstrap-backend-play-28" % "5.24.0",
-    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28"        % "0.62.0",
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28"        % "0.68.0",
     "org.julienrf"      %% "play-json-derived-codecs"  % "7.0.0",
     "org.typelevel"     %% "cats-core"                 % "2.7.0",
     "com.lucidchart"    %% "xtract"                    % "2.3.0-alpha3",
@@ -18,7 +18,7 @@ object AppDependencies {
     "org.scalatestplus"      %% "scalatestplus-scalacheck" % "3.1.0.0-RC2"       % Test,
     "wolfendale"             %% "scalacheck-gen-regexp"    % "0.1.2"             % Test,
     "uk.gov.hmrc"            %% "bootstrap-test-play-28"   % "5.21.0"            % Test,
-    "uk.gov.hmrc.mongo"      %% "hmrc-mongo-test-play-28"  % "0.55.0"            % Test,
+    "uk.gov.hmrc.mongo"      %% "hmrc-mongo-test-play-28"  % "0.68.0"            % Test,
     "org.mockito"            %% "mockito-scala"            % "1.10.6"            % Test,
     "com.vladsch.flexmark"    % "flexmark-all"             % "0.36.8"            % "test, it",
     "org.scalatest"          %% "scalatest"                % "3.1.0"             % Test,
