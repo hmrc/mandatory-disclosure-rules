0aca391481ab87bf33c20f5fa95d6bd01a25cf15
AashuChauhanHmrc <aashu.chauhan@digital.hmrc.gov.uk>
2023-05-19 12:04:13 +0100

    scoveraged changed to 90

--- project/ScoverageSettings.scala
+++ project/ScoverageSettings.scala
@@ -16,7 +16,7 @@ object ScoverageSettings {
 
   val settings: Seq[Setting[_]] = Seq(
     ScoverageKeys.coverageExcludedPackages := excludedPackages.mkString(";"),
-    ScoverageKeys.coverageMinimumStmtTotal := 80,
+    ScoverageKeys.coverageMinimumStmtTotal := 90,
     ScoverageKeys.coverageFailOnMinimum := true,
     ScoverageKeys.coverageHighlighting := true
   )
